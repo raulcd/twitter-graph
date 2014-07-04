fd8cff811f20ec9c376094bd2ab459dd13264e87
Raúl Cumplido <raulcumplido@gmail.com>
2014-07-04 18:05:46 +0100

    Modified twitter api to be a class and nodedict to add timezone if it is not None

--- twitter_graph.py
+++ twitter_graph.py
@@ -6,41 +6,52 @@ SECRETS_FILE = 'secrets.txt'
 NEO4J_DB = 'http://localhost:7474/db/data/'
 
 
+class TwitterAPI(object):
+    """docstring for TwitterAPI"""
+
+    def __init__(self, key, secret):
+        super(TwitterAPI, self).__init__()
+        self.api = self.get_twitter_api(key, secret)
+
+    def get_twitter_api(self, key, secret):
+        auth = tweepy.AppAuthHandler(key, secret)
+        api = tweepy.API(auth)
+        return api
+
+    def get_followers(self, user):
+        """
+        docstring for get_twitter_followers
+        Get twitter followers from a user and returns the list of followers
+        """
+        return tweepy.Cursor(self.api.followers, id=user)
+
+    def get_followed(self, user):
+        """
+        Get the followed users from a user
+        """
+        return tweepy.Cursor(self.api.friends, id=user)
+
+    def get_twitter_connections(self, user):
+        friends = self.get_followed(user)
+        followers = self.get_followers(user)
+        return {'user': user, 'friends': friends, 'followers': followers}
+
+
 def read_secrets():
     twitter_key, twitter_secret = open(SECRETS_FILE).readlines()
     return twitter_key[:-1], twitter_secret[:-1]
 
 
-def get_twitter_api(key, secret):
-    auth = tweepy.AppAuthHandler(key, secret)
-    api = tweepy.API(auth)
-    return api
-
-
-def get_followers(api, user):
-    """
-    docstring for get_twitter_followers
-    Get twitter followers from a user and returns the list of followers
-    """
-    return tweepy.Cursor(api.followers, id=user)
-
-
-def get_followed(api, user):
-    """
-    Get the followed users from a user
-    """
-    return tweepy.Cursor(api.friends, id=user)
-
-
-def get_twitter_connections(user):
-    api = get_twitter_api(*read_secrets())
-    friends = get_followed(api, user)
-    followers = get_followers(api, user)
-    return {'user': user, 'friends': friends, 'followers': followers}
-
-
 def start_database():
     return neo4j.GraphDatabaseService(NEO4J_DB)
+
+
+def create_node_dict(twitter_user):
+    node = {'name': twitter_user.screen_name,
+            'location': twitter_user.location,
+            'time_zone': twitter_user.time_zone}
+    # Remove None values
+    return node.update((k, v) for k, v in node.iteritems() if v is not None)
 
 
 def update_nodes(graph_db, user_connections):
@@ -50,9 +61,7 @@ def update_nodes(graph_db, user_connections):
     count = 0
     for friend in user_connections['friends'].items():
         friend_node = people.get_or_create('name', friend.screen_name,
-                                           {'name': friend.screen_name,
-                                            'location': friend.location,
-                                            'timezone': friend.timezone})
+                                           create_node_dict(friend))
         user_node.create_path("FOLLOWS", friend_node)
         if count == 0:
             count += 1
@@ -61,14 +70,13 @@ def update_nodes(graph_db, user_connections):
                 print "Key: %s, Value: %s" % (k, v)
     for follower in user_connections['followers'].items():
         follower_node = people.get_or_create('name', follower.screen_name,
-                                             {'name': follower.screen_name,
-                                              'location': follower.location,
-                                              'timezone': follower.timezone})
+                                             create_node_dict(follower))
         follower_node.create_path("FOLLOWS", user_node)
 
 
 if __name__ == '__main__':
     graph_db = start_database()
     user = raw_input("What's the username you want to check?")
-    user_connections = get_twitter_connections(user)
+    twitter_api = TwitterAPI(*read_secrets())
+    user_connections = twitter_api.get_twitter_connections(user)
     update_nodes(graph_db, user_connections)
