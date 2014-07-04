fe1f47fec53999b2146355d421e360da934808da
Raúl Cumplido <raulcumplido@gmail.com>
2014-07-04 11:22:19 +0100

    Recover followers and followed from an entered user

--- twitter_graph.py
+++ twitter_graph.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python
+import tweepy
 
-def get_followers(user):
+SECRETS_FILE = 'secrets.txt'
+
+
+def read_secrets():
+    twitter_key, twitter_secret = open(SECRETS_FILE).readlines()
+    return twitter_key[:-1], twitter_secret[:-1]
+
+
+def get_twitter_api(key, secret):
+    auth = tweepy.AppAuthHandler(key, secret)
+    api = tweepy.API(auth)
+    return api
+
+
+def get_followers(api, user):
     """
     docstring for get_twitter_followers
     Get twitter followers from a user and returns the list of followers
-    
     """
+    return api.followers(id=user)
     pass
 
-def get_followed(user):
+
+def get_followed(api, user):
     """
     Get the followed users from a user
     """
-    info = get_twitter_info(user)
-    return info
+    return api.friends(id=user)
 
-def get_twitter_info(user):
-    """
-    Get the twitter info for a given user
-    """
-    #TODO Request to twitter to get info
-    pass
 
 if __name__ == '__main__':
     user = raw_input("What's the username you want to check?")
-    print get_followed(user)
+    api = get_twitter_api(*read_secrets())
+    friends = get_followed(api, user)
+    print "Follows"
+    for friend in friends:
+        print friend.name
+    print "Followers"
+    followers = get_followers(api, user)
+    for follower in followers:
+        print follower.name
