a44470293d8f418a9ba830e62592ba00062c03cc
Raúl Cumplido <raulcumplido@gmail.com>
2014-07-04 14:13:17 +0100

    First graph of followers and friends

--- twitter_graph.py
+++ twitter_graph.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import tweepy
+from py2neo import neo4j, node
 
 SECRETS_FILE = 'secrets.txt'
+NEO4J_DB = 'http://localhost:7474/db/data/'
 
 
 def read_secrets():
@@ -30,15 +32,34 @@ def get_followed(api, user):
     return tweepy.Cursor(api.friends, id=user)
 
 
-if __name__ == '__main__':
-    user = raw_input("What's the username you want to check?")
+def get_twitter_connections(user):
     api = get_twitter_api(*read_secrets())
-    cursor = 0
     friends = get_followed(api, user)
-    print "Follows"
-    for friend in friends.items():
-        print friend.name
-    print "Followers"
     followers = get_followers(api, user)
-    for follower in followers.items():
-        print follower.name
+    return {'user': user, 'friends': friends, 'followers': followers}
+
+
+def start_database():
+    return neo4j.GraphDatabaseService(NEO4J_DB)
+
+
+def update_nodes(graph_db, user_connections):
+    people = graph_db.get_or_create_index(neo4j.Node, 'people')
+    user_node = people.get_or_create('name', user_connections['user'],
+            {'name':  user_connections['user']})
+    for friend in user_connections['friends'].items():
+        friend_node = people.get_or_create('name', friend.screen_name,
+                {'name': friend.screen_name})
+        user_node.create_path("FOLLOWS", friend_node)
+    for follower in user_connections['followers'].items():
+        follower_node = people.get_or_create('name', follower.screen_name,
+                {'name': follower.screen_name})
+        follower_node.create_path("FOLLOWS", user_node)
+
+
+if __name__ == '__main__':
+    graph_db = start_database()
+    print(graph_db.neo4j_version)
+    user = raw_input("What's the username you want to check?")
+    user_connections = get_twitter_connections(user)
+    update_nodes(graph_db, user_connections)
