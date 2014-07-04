387839423cada67dd3fd1ca54d89098d658ffe4a
Raúl Cumplido <raulcumplido@gmail.com>
2014-07-04 20:16:00 +0100

    Added generation of CSV with location to import in cartodb

--- twitter_graph.py
+++ twitter_graph.py
@@ -36,6 +36,9 @@ class TwitterAPI(object):
         followers = self.get_followers(user)
         return {'user': user, 'friends': friends, 'followers': followers}
 
+    def get_user_timeline(self, user):
+        return self.api.user_timeline(id=user)
+
 
 def read_secrets():
     twitter_key, twitter_secret = open(SECRETS_FILE).readlines()
@@ -46,32 +49,68 @@ def start_database():
     return neo4j.GraphDatabaseService(NEO4J_DB)
 
 
-def create_node_dict(twitter_user):
-    node = {'name': twitter_user.screen_name,
-            'location': twitter_user.location,
-            'time_zone': twitter_user.time_zone}
+def create_node_dict(twitter_user, twitter_api):
+    node = {'name': twitter_user.screen_name}
+    if not twitter_user.time_zone and not twitter_user.location:
+        pass
+        """geo = get_last_tweet_location(twitter_api, twitter_user)
+        if geo:
+            node.update({'geo': geo})"""
+    else:
+        if twitter_user.time_zone:
+            node.update({'time_zone': twitter_user.time_zone})
+        elif twitter_user.location:
+            node.update({'location': twitter_user.location})
     # Remove None values
-    return node.update((k, v) for k, v in node.iteritems() if v is not None)
+    return node
 
 
-def update_nodes(graph_db, user_connections):
+def get_last_tweet_location(twitter_api, friend):
+    try:
+        timeline = twitter_api.get_user_timeline(friend.id)
+        for tweet in timeline:
+            if tweet.geo:
+                print tweet.geo, friend.screen_name
+                return tweet.geo
+    except tweepy.TweepError:
+        # This user has a private timeline
+        pass
+
+
+def update_nodes(graph_db, user_connections, twitter_api):
     people = graph_db.get_or_create_index(neo4j.Node, 'people')
     user_node = people.get_or_create('name', user_connections['user'],
                                      {'name':  user_connections['user']})
-    count = 0
+    # count = 0
     for friend in user_connections['friends'].items():
         friend_node = people.get_or_create('name', friend.screen_name,
-                                           create_node_dict(friend))
+                                           create_node_dict(friend,
+                                                            twitter_api))
         user_node.create_path("FOLLOWS", friend_node)
-        if count == 0:
+        """if count == 0:
             count += 1
             dict_attributes = vars(friend)
             for k, v in dict_attributes.iteritems():
-                print "Key: %s, Value: %s" % (k, v)
+                print "Key: %s, Value: %s" % (k, v)"""
     for follower in user_connections['followers'].items():
         follower_node = people.get_or_create('name', follower.screen_name,
-                                             create_node_dict(follower))
+                                             create_node_dict(follower,
+                                                              twitter_api))
         follower_node.create_path("FOLLOWS", user_node)
+
+
+def create_map_csv(graph_db, filename):
+    my_file = open(filename, 'w')
+    query = neo4j.CypherQuery(graph_db,
+                              "Match n return n.name, n.location, n.time_zone")
+    for record in query.stream():
+        if record[1]:
+            my_file.write('{0},{1}\n'.format(record[0].encode('utf-8'),
+                                             record[1].encode('utf-8')))
+        elif record[2]:
+            my_file.write('{0},{1}\n'.format(record[0].encode('utf-8'),
+                                             record[2].encode('utf-8')))
+    my_file.close()
 
 
 if __name__ == '__main__':
@@ -79,4 +118,5 @@ if __name__ == '__main__':
     user = raw_input("What's the username you want to check?")
     twitter_api = TwitterAPI(*read_secrets())
     user_connections = twitter_api.get_twitter_connections(user)
-    update_nodes(graph_db, user_connections)
+    update_nodes(graph_db, user_connections, twitter_api)
+    create_map_csv(graph_db, "file.csv")
